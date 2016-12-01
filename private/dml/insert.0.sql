d244c648075a973673b7022365555bf6aeb825cc
Alexander Verbruggen <alex@nabu.be>
2016-12-01 15:42:06 +0100

    Initial

---
+++ private/dml/insert.0.sql
@@ -0,0 +1,11 @@
+insert into components (
+	id,
+	created,
+	modified,
+	name
+) values (
+	'a933de5a-fd23-46e5-a3fe-1dac853c85d5',
+	timestamp '2016-11-22 12:18:23.000',
+	timestamp '2016-11-22 12:18:23.000',
+	'order'
+);
